ceaf096832273eb9da82058e1006c5cf8ac18f1c
ramiromiglio <rami.mig.dev@gmail.com>
2024-06-28 04:26:58 -0300

    Corrigidos los códigos de error de respuesta

--- source/errors.js
+++ source/errors.js
@@ -1,14 +1,14 @@
 class APIError extends Error {
     static ERR_USERNAME_CONFLICT  = () => new APIError(409, '/auth/username-conflict');
     static ERR_USERNAME_NOT_FOUND = () => new APIError(404, '/auth/username-not-found');
-    static ERR_INVALID_PASSWORD   = () => new APIError(400, '/auth/invalid-password');
+    static ERR_INVALID_PASSWORD   = () => new APIError(401, '/auth/invalid-password');
     static ERR_ACCESS_DENIED      = () => new APIError(403, '/auth/access-denied');
-    static ERR_INVALID_TOKEN      = () => new APIError(500, '/auth/invalid-token');
-    static ERR_INVALID_SCHEMA     = (details) => new APIError(500, '/invalid-schema', details);
+    static ERR_INVALID_TOKEN      = () => new APIError(401, '/auth/invalid-token');
+    static ERR_INVALID_SCHEMA     = (details) => new APIError(400, '/invalid-schema', details);
     static ERR_INTERNAL_SERVER    = () => new APIError(500, '/internal-server');
-    static ERR_FILE_NOT_FOUND     = () => new APIError(500, '/storage/file-not-found');
-    static ERR_EXPIRED_TOKEN      = () => new APIError(500, '/storage/expired-token');
-    static ERR_USED_TOKEN         = () => new APIError(500, '/storage/used-token');
+    static ERR_FILE_NOT_FOUND     = () => new APIError(404, '/storage/file-not-found');
+    static ERR_EXPIRED_TOKEN      = () => new APIError(401, '/storage/expired-token');
+    static ERR_USED_TOKEN         = () => new APIError(401, '/storage/used-token');
 
     constructor(statusCode, type, details = undefined) {
         super();
